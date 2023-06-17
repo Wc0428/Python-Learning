0d8668224f0bf33f1b22c5dbf4bb5a6f980bbaba
Wc0428 <109028745+Wc0428@users.noreply.github.com>
2023-06-18 00:45:39 +0800

    data-types

---
+++ data_types.py
@@ -0,0 +1,11 @@
+#string
+#int
+#float
+#boolean
+# num1 = int(input("enter"))
+# num2 = float(input("enter"))
+# print(num1 + num2)
+
+num = input("Please insert 2 digital number: ")
+print(int(num[0])+int(num[1]))
+
